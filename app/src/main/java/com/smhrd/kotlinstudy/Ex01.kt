1aedd80e5fa03faba9e1d9f805d7760135a7a195
천현민 <poppy3672@naver.com>
2023-08-16 20:56:11 +0900

    commit

--- app/src/main/java/com/smhrd/kotlinstudy/Ex01.kt
+++ app/src/main/java/com/smhrd/kotlinstudy/Ex01.kt
@@ -4,7 +4,6 @@ fun main(){
     val americano = Americano().apply {
         setTemperature(Temperature.Hot)
         setVanilliaCream(CoffeeOption.Vanilla)
-
     }
 
     val latte = Latte().apply {
@@ -17,35 +16,106 @@ fun main(){
         setVanilliaCream(CoffeeOption.None)
         this
     }
-
-    val jamongade = Ade().apply {
-
-    }
-
-    val honeyblack = HoneyBlack().apply {
-
-    }
-
-
-
+//
+//    val jamongade = Ade().apply {
+//
+//    }
+//
+//    val honeyblack = HoneyBlack().apply {
+//
+//    }
+//
     americano.getMenu() // 4000원 Vanilla Hot 아메리카노 나왔습니다!
     latte.getMenu()
     vanillalatte.getMenu()
-    jamongade.getMenu()
-    honeyblack.getMenu()
+//    jamongade.getMenu()
+//    honeyblack.getMenu()
 
 }
 
 
 
-interface Coffee{
+//interface Coffee {
+//    var price : Int
+//    val name : String
+//
+//    fun getMenu()
+//}
+//
+//
+
+//
+//
+//abstract class CoffeeMenu :Coffee{
+//    var selectTem : Temperature = Temperature.Hot
+//    var selectOption : CoffeeOption = CoffeeOption.Vanilla
+//
+//    open fun setTemperature(tem : Temperature) {
+//        selectTem = tem
+//    }
+//
+//    open fun setVanilliaCream(option : CoffeeOption){
+//        selectOption = option
+//    }
+//
+//    override fun getMenu() {
+//        println("${price}원 ${selectOption} ${selectTem}${name}나왔습니다!")
+//    }
+//}
+//
+//class Americano() : CoffeeMenu() {
+//    override val name : String = "아메리카노"
+//    override var price : Int = 4000
+//}
+//
+
+
+//
+//class VanillaCreamF() : CoffeeMenu(){
+//    override val name : String = "바닐라 크림 프라푸치노"
+//    override var price : Int = 4000
+//
+//}
+//
+//open class Jamong() : CoffeeMenu(){
+//    override val name : String = "자몽"
+//    override var price : Int = 4000
+//
+//    override fun getMenu() {
+//        println("${price} 원 ${name}나왔습니다!")
+//    }
+//}
+
+//class Ade() : Jamong() {
+//    override val name: String
+//        get() = "자몽 에이드"
+//    override var price : Int = 5000
+//
+//}
+//
+//class HoneyBlack() : Jamong(){
+//    override val name: String
+//        get() = "자몽 허니 블랙티"
+//    override var price : Int = 5000
+//}
+
+//Coffee: Menu()
+//Americano: Coffee()
+//Latte: Coffee()
+//
+//Ade: Menu()
+//JamongAde: Ade()
+//
+//Tea: Menu()
+//JamongHoneyBlackTea: Tea()
+
+
+interface Menu {
     var price : Int
     val name : String
 
     fun getMenu()
 }
-
-
 
 enum class Temperature{
     Hot , Ice
@@ -55,72 +125,78 @@ enum class CoffeeOption{
     Vanilla, Almond, None
 }
 
+abstract class Coffee : Menu{
+    override var price : Int =0
+    override val name : String = ""
 
-abstract class CoffeeMenu :Coffee{
+    open var tem : Temperature? = Temperature.Hot
+    open var option : CoffeeOption? = CoffeeOption.Vanilla
 
-    var selectTem : Temperature = Temperature.Hot
-    var selectOption : CoffeeOption = CoffeeOption.Vanilla
-
-    open fun setTemperature(tem : Temperature) {
-        selectTem = tem
+    fun setTemperature(tem : Temperature) {
+        this.tem = tem
     }
 
-    open fun setVanilliaCream(option : CoffeeOption){
-        selectOption = option
+    fun setVanilliaCream(option : CoffeeOption){
+        this.option = option
     }
 
-    override fun getMenu() {
-        println("${price}원 ${selectOption} ${selectTem}${name}나왔습니다!")
+    override fun getMenu(){
+        println("${price}원 ${option} ${tem} ${name} 나왔습니다.")
     }
+
 
 }
 
-class Americano() : CoffeeMenu() {
+abstract class Tea : Menu{
+    override var price : Int =0
+    override val name : String = ""
+
+
+}
+
+class Americano() : Coffee() {
+    override var price: Int = 4000
     override val name : String = "아메리카노"
-    override var price : Int = 4000
+
 }
 
-class Latte() : CoffeeMenu() {
+class Latte() : Coffee(){
     override val name : String = "카페 라뗴"
     override var price : Int = 4000
-}
-
-class VanillaLatte() : CoffeeMenu(){
-    override val name : String = "바닐라 라떼"
-    override var price : Int = 4000
-
-}
-
-class VanillaCreamF() : CoffeeMenu(){
-    override val name : String = "바닐라 크림 프라푸치노"
-    override var price : Int = 4000
-
-}
-
-open class Jamong() : CoffeeMenu(){
-
-    override val name : String = "자몽"
-    override var price : Int = 4000
+    override var tem : Temperature? = null
 
     override fun getMenu() {
-        println("${price} 원 ${name}나왔습니다!")
+        tem.let {
+            println("${price}원 ${option} ${tem} ${name} 나왔습니다.")
+        } ?: println("${price}원 ${tem} ${name} 나왔습니다.")
+
     }
-
-
 }
 
-class Ade() : Jamong() {
-    override val name: String
-        get() = "자몽 에이드"
-    override var price : Int = 5000
 
+class VanillaLatte() : Coffee(){
+    override val name : String = "바닐라 라떼"
+    override var price : Int = 4000
 }
 
-class HoneyBlack() : Jamong(){
-    override val name: String
-        get() = "자몽 허니 블랙티"
-    override var price : Int = 5000
+class VanillaCreamF() : Coffee(){
+    override val name : String = "바닐라 크림 프라푸치노"
+    override var price : Int =  4000
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/smhrd/kotlinstudy/other.kt
+++ app/src/main/java/com/smhrd/kotlinstudy/other.kt
@@ -1,66 +1,66 @@
-package com.smhrd.kotlinstudy
-
-
-// 추상클래스로 처리를 한 경우
-
-//커피가 들어간 메뉴는 다음과 같다.
-//open class => 그대로 사용이 가능함
-//abstract class => 반드시 구현해야함
-abstract class CoffeeMenu1 : Menu {
-
-    //Menu interface // 이름과 가격은 메뉴의 종류별로 다르게 나타남
-    abstract override var price: Int
-    abstract override val name: String
-
-    //해당 메서드는 모든 메뉴가 동일하게 제공됨
-    override fun getMenu() {
-        println( "$temperature $name 나왔습니다. 가격은 ${price}원 입니다" )
-    }
-
-    abstract var temperature : Temperature
-    // enum class
-
-    //커피 관련 음료는 핫/아이스 옵션 선택이 가능 하다.
-    enum class Temperature{
-        Hot, Ice
-    }
-}
-
-
-enum class CoffeeOption1 {
-    Vanilla, Almond, None
-}
-
-class Latte1() : CoffeeMenu1(){
-    override var price: Int
-        get() = 4000
-        set(value) {}
-    override val name: String
-        get() = "카페 라떼"
-
-    override var temperature : Temperature = Temperature.Hot
-//    override var coffeeOption: Option = Option.None
-
-}
-
-class Americano1() : CoffeeMenu1() {
-
-    override var price: Int = 4000
-    override val name: String = "아메리카노"
-    // coffeMenu라면 반드시 입력되어야 하는 정보
-
-    override var temperature: Temperature = Temperature.Hot  // 온도 설정에 기본 값
-
-    var coffeeOption : CoffeeOption = CoffeeOption.None
-
-}
-
-
-//메뉴의 공통 요소 ( 전 메뉴 )
-interface Menu{
-    var price : Int
-    val name : String
-    fun getMenu()
-}
-
-// 모든 프로퍼티가 다형성을 가지고 있음 => 추상은 부모클래스의 요소를 가져다 쓰는게 있을때 더 어울림
+//package com.smhrd.kotlinstudy
+//
+//
+//// 추상클래스로 처리를 한 경우
+//
+////커피가 들어간 메뉴는 다음과 같다.
+////open class => 그대로 사용이 가능함
+////abstract class => 반드시 구현해야함
+//abstract class CoffeeMenu1 : Menu {
+//
+//    //Menu interface // 이름과 가격은 메뉴의 종류별로 다르게 나타남
+//    abstract override var price: Int
+//    abstract override val name: String
+//
+//    //해당 메서드는 모든 메뉴가 동일하게 제공됨
+//    override fun getMenu() {
+//        println( "$temperature $name 나왔습니다. 가격은 ${price}원 입니다" )
+//    }
+//
+//    abstract var temperature : Temperature
+//    // enum class
+//
+//    //커피 관련 음료는 핫/아이스 옵션 선택이 가능 하다.
+//    enum class Temperature{
+//        Hot, Ice
+//    }
+//}
+//
+//
+//enum class CoffeeOption1 {
+//    Vanilla, Almond, None
+//}
+//
+//class Latte1() : CoffeeMenu1(){
+//    override var price: Int
+//        get() = 4000
+//        set(value) {}
+//    override val name: String
+//        get() = "카페 라떼"
+//
+//    override var temperature : Temperature = Temperature.Hot
+////    override var coffeeOption: Option = Option.None
+//
+//}
+//
+//class Americano1() : CoffeeMenu1() {
+//
+//    override var price: Int = 4000
+//    override val name: String = "아메리카노"
+//    // coffeMenu라면 반드시 입력되어야 하는 정보
+//
+//    override var temperature: Temperature = Temperature.Hot  // 온도 설정에 기본 값
+//
+//    var coffeeOption : CoffeeOption = CoffeeOption.None
+//
+//}
+//
+//
+////메뉴의 공통 요소 ( 전 메뉴 )
+//interface Menu{
+//    var price : Int
+//    val name : String
+//    fun getMenu()
+//}
+//
+//// 모든 프로퍼티가 다형성을 가지고 있음 => 추상은 부모클래스의 요소를 가져다 쓰는게 있을때 더 어울림
