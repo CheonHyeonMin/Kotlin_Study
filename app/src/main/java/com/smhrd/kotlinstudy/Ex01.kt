315d944430e6c3ada41e452c6483bfc0d5022967
천현민 <poppy3672@naver.com>
2023-08-17 14:12:04 +0900

    commit

--- app/src/main/java/com/smhrd/kotlinstudy/Ex01.kt
+++ app/src/main/java/com/smhrd/kotlinstudy/Ex01.kt
@@ -17,6 +17,10 @@ fun main(){
         this
     }
 
+    val vanillaCreamF = VanillaCreamF().apply {
+
+    }
+
     val jamongade = JamongAde().apply {
 
     }
@@ -28,87 +32,11 @@ fun main(){
     americano.getMenu() // 4000원 Vanilla Hot 아메리카노 나왔습니다!
     latte.getMenu()
     vanillalatte.getMenu()
+    vanillaCreamF.getMenu()
     jamongade.getMenu()
     honeyblack.getMenu()
 
 }
-
-
-
-//interface Coffee {
-//    var price : Int
-//    val name : String
-//
-//    fun getMenu()
-//}
-//
-//
-
-//
-//
-//abstract class CoffeeMenu :Coffee{
-//    var selectTem : Temperature = Temperature.Hot
-//    var selectOption : CoffeeOption = CoffeeOption.Vanilla
-//
-//    open fun setTemperature(tem : Temperature) {
-//        selectTem = tem
-//    }
-//
-//    open fun setVanilliaCream(option : CoffeeOption){
-//        selectOption = option
-//    }
-//
-//    override fun getMenu() {
-//        println("${price}원 ${selectOption} ${selectTem}${name}나왔습니다!")
-//    }
-//}
-//
-//class Americano() : CoffeeMenu() {
-//    override val name : String = "아메리카노"
-//    override var price : Int = 4000
-//}
-//
-
-
-//
-//class VanillaCreamF() : CoffeeMenu(){
-//    override val name : String = "바닐라 크림 프라푸치노"
-//    override var price : Int = 4000
-//
-//}
-//
-//open class Jamong() : CoffeeMenu(){
-//    override val name : String = "자몽"
-//    override var price : Int = 4000
-//
-//    override fun getMenu() {
-//        println("${price} 원 ${name}나왔습니다!")
-//    }
-//}
-
-//class Ade() : Jamong() {
-//    override val name: String
-//        get() = "자몽 에이드"
-//    override var price : Int = 5000
-//
-//}
-//
-//class HoneyBlack() : Jamong(){
-//    override val name: String
-//        get() = "자몽 허니 블랙티"
-//    override var price : Int = 5000
-//}
-
-//Coffee: Menu()
-//Americano: Coffee()
-//Latte: Coffee()
-//
-//Ade: Menu()
-//JamongAde: Ade()
-//
-//Tea: Menu()
-//JamongHoneyBlackTea: Tea()
-
 
 interface Menu {
     var price : Int
@@ -150,14 +78,14 @@ abstract class Tea : Menu{
 
 
     override fun getMenu() {
-        println("${price} 원 ${name}나왔습니다.")
+        println("${price}원 ${name}나왔습니다.")
     }
 }
 
 
 abstract class Ade : Menu{
     override fun getMenu() {
-        println("${price} 원 ${name}나왔습니다.")
+        println("${price}원 ${name}나왔습니다.")
     }
 }
 
@@ -176,9 +104,7 @@ class Latte() : Coffee(){
     override var tem : Temperature? = null
 
     override fun getMenu() {
-        tem.let {
-            println("${price}원 ${option} ${tem} ${name} 나왔습니다.")
-        } ?: println("${price}원 ${tem} ${name} 나왔습니다.")
+        println("${price}원 ${option} ${tem} ${name} 나왔습니다.")
 
     }
 }
@@ -189,7 +115,7 @@ class VanillaLatte() : Coffee(){
     override var price : Int = 4000
 
     override fun getMenu() {
-        println("${price}원 ${tem} ${name} 나왔습니다.")
+        println("${price}원 ${option} ${tem} ${name} 나왔습니다.")
     }
 
 }
@@ -199,7 +125,7 @@ class VanillaCreamF() : Coffee(){
     override var price : Int =  4000
 
     override fun getMenu() {
-        println("${price}원 ${tem} ${name} 나왔습니다.")
+        println("${price}원 ${name} 나왔습니다.")
     }
 }
 
